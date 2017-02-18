e07c784fc6259a7d60586c24f26798ac781254c9
Aesylwinn <kyleacooley@gmail.com>
2017-02-18 01:41:11 -0500

    Basic transmission class outline

---
+++ app/src/main/java/com/sh/blah/dotdash/MorseTransmission.java
@@ -0,0 +1,28 @@
+package com.sh.blah.dotdash;
+
+import android.hardware.Camera;
+
+import java.util.Map;
+
+/**
+ * Created by aesylwinn on 2/18/17.
+ */
+
+public class MorseTransmission {
+
+    MorseTransmission(){
+    }
+
+    public void pulse(int length)
+    {
+    }
+
+    public void transmit(Camera camera, String message)
+    {
+    }
+
+    public String recieve(Camera camera)
+    {
+        return "";
+    }
+}
