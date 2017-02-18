8243572a234af09e95d368fc8615e898de585988
royalardor <paulpupo@yahoo.com>
2017-02-18 05:59:46 -0500

    Began read activity

--- app/src/main/java/com/sh/blah/dotdash/MainActivity.java
+++ app/src/main/java/com/sh/blah/dotdash/MainActivity.java
@@ -2,7 +2,9 @@ package com.sh.blah.dotdash;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -11,10 +13,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainActivity extends AppCompatActivity {
 
     MorseTransmission transmitter;
     EditText morseText;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,31 +37,67 @@ public class MainActivity extends AppCompatActivity {
         morseText = (EditText) findViewById(R.id.morseTextField);
 
 
-
         // Request permissions for Android 6
         int camPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
-        if (camPermission != PackageManager.PERMISSION_GRANTED)
-        {
-            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA }, 1 );
+        if (camPermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
         }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
     //HELLO FOOLS
-    public void SendButtonClick (View v){
+    public void SendButtonClick(View v) {
         transmitter = new MorseTransmission();
         transmitter.transmit(morseText.getText().toString());
     }
 
-
     //Read Button Click
-    public void ReadButtonClick (View v){
-        setContentView(R.layout.activity_read);
-        MorseRead read = new MorseRead();
-        read.StartCamera();
+    public void ReadButtonClick(View v) {
+        Intent i = new Intent(this, MorseReader.class);
+        startActivity(i);
     }
 
     @Override
     public void onRequestPermissionsResult(int reqCode, String[] permissions, int[] results) {
         super.onRequestPermissionsResult(reqCode, permissions, results);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
 
---
+++ app/src/main/java/com/sh/blah/dotdash/MorseReader.java
@@ -0,0 +1,36 @@
+package com.sh.blah.dotdash;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+public class MorseReader extends AppCompatActivity {
+    private SurfaceView sView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_morse_reader);
+        sView = (SurfaceView)findViewById(R.id.surfaceView);
+    }
+
+    @Override
+    protected void  onResume(){
+        super.onResume();
+        StartCamera(this);
+    }
+    void StartCamera(Activity act){
+        Camera cam = Camera.open();
+        cam.setDisplayOrientation(0);
+        try {
+            cam.setPreviewDisplay(sView.getHolder());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        cam.startPreview();
+    }
+}
