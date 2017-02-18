fdd1d75be0de1a3d5c4039c32fc817bd989db425
Aesylwinn <kyleacooley@gmail.com>
2017-02-18 02:02:45 -0500

    Pulse function work

--- app/src/main/java/com/sh/blah/dotdash/MainActivity.java
+++ app/src/main/java/com/sh/blah/dotdash/MainActivity.java
@@ -3,18 +3,24 @@ package com.sh.blah.dotdash;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    MorseTransmission transmitter;
+    EditText morseText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        morseText = (EditText) findViewById(R.id.morseTextField);
     }
 
     public void SendButtonClick (View v){
 
-
+        transmitter.transmit("blah");
     }
 }
 
--- app/src/main/java/com/sh/blah/dotdash/MorseTransmission.java
+++ app/src/main/java/com/sh/blah/dotdash/MorseTransmission.java
@@ -13,12 +13,32 @@ public class MorseTransmission {
     MorseTransmission(){
     }
 
-    public void pulse(int length)
+
+    /**
+     * Pulses the camera flashlight
+     * @param camera the camera object
+     * @param length the duration in milliseconds
+     */
+    public void pulse(Camera camera, int length)
     {
+        camera.startPreview();
+        try {
+            Thread.sleep(length);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        camera.stopPreview();
     }
 
-    public void transmit(Camera camera, String message)
+    public void transmit(String message)
     {
+        Camera camera = Camera.open();
+        Camera.Parameters parameters = camera.getParameters();
+
+        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        camera.setParameters(parameters);
+
+        pulse(camera, 1000);
     }
 
     public String recieve(Camera camera)
