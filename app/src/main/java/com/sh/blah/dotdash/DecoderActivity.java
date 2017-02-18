ea073597acd82c4c925233b1dd2954983427eb25
Aesylwinn <kyleacooley@gmail.com>
2017-02-18 16:41:13 -0500

    Detection

--- app/src/main/java/com/sh/blah/dotdash/DecoderActivity.java
+++ app/src/main/java/com/sh/blah/dotdash/DecoderActivity.java
@@ -2,6 +2,7 @@ package com.sh.blah.dotdash;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,6 +19,9 @@ import java.io.IOException;
 import java.util.List;
 
 public class DecoderActivity extends Activity implements Camera.PreviewCallback {
+
+    final int ExaminedWidth = 500 * 3;
+    final int ExaminedHeight = 500 * 3;
 
     private Camera mCamera = null;
     private CameraView mCameraView = null;
@@ -42,7 +46,30 @@ public class DecoderActivity extends Activity implements Camera.PreviewCallback 
 
     @Override
     public void onPreviewFrame(byte[] data, Camera camera) {
-        Log.d("data", Integer.toString(data.length));
+        Camera.Size size = camera.getParameters().getPreviewSize();
+
+        int scalar = 3; // YUV - nv21, 3 bytes
+
+        int xOffset = 0;
+        int yOffset = 0;
+
+        int xMax = Math.max(0, Math.min(size.width - 1, xOffset + ExaminedWidth));
+        int yMax = Math.max(0, Math.min(size.height - 1, yOffset + ExaminedHeight));
+
+
+        double brightness = 0;
+        int count = 1;
+
+        for (int y = yOffset; y < yMax; y += 1)
+        {
+            for (int x = xOffset; x < xMax; x += scalar)
+            {
+                brightness += data[x + y * size.width] & 0xFF;
+                ++count;
+            }
+        }
+        brightness /= count;
+        Log.d("bright", Double.toString(brightness));
     }
 
     public class CameraView extends SurfaceView implements SurfaceHolder.Callback{
@@ -54,6 +81,13 @@ public class DecoderActivity extends Activity implements Camera.PreviewCallback 
 
             mCamera = camera;
             mCamera.setDisplayOrientation(90);
+            Camera.Parameters parameters = camera.getParameters();
+            parameters.setPreviewFormat(ImageFormat.NV21);
+            if (parameters.isAutoWhiteBalanceLockSupported())
+                parameters.setAutoWhiteBalanceLock(true);
+            if (parameters.isAutoExposureLockSupported())
+                parameters.setAutoExposureLock(true);
+            camera.setParameters(parameters);
             mHolder = getHolder();
             mHolder.addCallback(this);
             mHolder.setType(SurfaceHolder.SURFACE_TYPE_NORMAL);
